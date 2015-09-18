890a1fb6fd93d7633ab1f74eb3831a4f32e81ce8
tommyfok <154274174@qq.com>
2015-09-18 19:02:27 +0800

    test

--- ngAutoComplete.js
+++ ngAutoComplete.js
@@ -4,11 +4,10 @@ angular.module('ngAutoComplete', [])
     restrict: 'AE',
     template: '<div class="nacContainer">'
              +'    <input placeholder="{{placeholder}}" type="{{type||\'text\'}}"'
-             +'           ng-model="input" class="form-control" ng-focus="showMorePkgs=true">'
+             +'           ng-value="input" class="form-control" ng-focus="showMorePkgs=true" ng-keypress="parse($event)">'
              +'    <div class="nacOuter" ng-show="showMorePkgs">'
              +'      <div class="nacInner">'
-             +'        <div ng-repeat="item in data | filter: input"'
-             +'             ng-click="set(item)" ng-bind-html="formatter(item)"></div>'
+             +'        <div ng-repeat="item in data | filter: inputArray[inputArray.length-1]" ng-click="set(item)" ng-bind="item"></div>'
              +'      </div>'
              +'    </div>'
              +'    <style>.nacContainer:hover .nacOuter{display: block;}.nacOuter{position: relative; display: none;}.nacInner{width: 100%; max-height: 250px; overflow: auto; position: absolute; left: 0; top: 0; z-index: 10; background: #fff; border: 1px solid #eee; border-top: none; box-shadow: 0 0 5px rgba(0,0,0,.1);}.nacInner > div{padding: 10px; cursor: pointer;}.nacInner > div:hover{background: #fafafa; color: #f60;}</style>'
@@ -17,23 +16,22 @@ angular.module('ngAutoComplete', [])
       input: '=ngModel',
       data: '=nacData',
       type: '@type',
-      placeholder: '@placeholder',
-      formatter: '=?nacFormatter',
-      parser: '=?nacParser'
+      placeholder: '@placeholder'
     },
     require: 'ngModel',
-    controller: function ($scope) {
-      $scope.formatter = angular.isFunction($scope.formatter) ? $scope.formatter : rawReturn;
-      $scope.parser = angular.isFunction($scope.parser) ? $scope.parser : rawReturn;
+    link: function (scope, elem, attrs, ngModelCtrl) {
+      scope.inputArray = [];
 
-      $scope.set = function (item) {
-        $scope.input = $scope.parser(item);
-        $scope.showMorePkgs = false;
+      scope.parse = function (event) {
+        scope.inputArray = scope.input.split(',');
       };
 
-      function rawReturn (item) {
-        return item;
-      }
+      scope.set = function (item) {
+        if (item) {
+          scope.inputArray.push(item);
+        }
+        scope.input = scope.inputArray.join(',');
+      };
     }
   };
 });
