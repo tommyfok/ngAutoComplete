b088c5beb41fbd152f717286f611e28e2dbbcaaf
tommyfok <154274174@qq.com>
2015-09-21 16:39:33 +0800

    test

--- ngAutoComplete.js
+++ ngAutoComplete.js
@@ -1,37 +1,67 @@
-angular.module('ngAutoComplete', [])
-.directive('ngAutoComplete', function () {
+angular.module('ngCombo', [])
+.directive('ngCombo', function () {
   return {
     restrict: 'AE',
-    template: '<div class="nacContainer">'
-             +'    <input placeholder="{{placeholder}}" type="{{type||\'text\'}}"'
-             +'           ng-value="input" class="form-control" ng-focus="showMorePkgs=true" ng-keypress="parse($event)">'
-             +'    <div class="nacOuter" ng-show="showMorePkgs">'
-             +'      <div class="nacInner">'
-             +'        <div ng-repeat="item in data | filter: inputArray[inputArray.length-1]" ng-click="set(item)" ng-bind="item"></div>'
+    template: '<div class="nacContainer" ng-mouseover="showUp=true" ng-mouseout="showUp=false">'
+             +'    <div class="input-group">'
+             +'      <input placeholder="{{placeholder}}" type="{{type||\'text\'}}"'
+             +'             ng-model="input" class="form-control" ng-focus="showMorePkgs=true">'
+             +'      <div class="input-group-btn">'
+             +'        <button type="button" class="btn btn-default" ng-click="focusInput()">'
+             +'          <span class="glyphicon glyphicon-chevron-down" ng-show="showUp&&showMorePkgs"></span>'
+             +'          <span class="glyphicon glyphicon-chevron-up" ng-hide="showUp&&showMorePkgs"></span>'
+             +'        </button>'
              +'      </div>'
              +'    </div>'
-             +'    <style>.nacContainer:hover .nacOuter{display: block;}.nacOuter{position: relative; display: none;}.nacInner{width: 100%; max-height: 250px; overflow: auto; position: absolute; left: 0; top: 0; z-index: 10; background: #fff; border: 1px solid #eee; border-top: none; box-shadow: 0 0 5px rgba(0,0,0,.1);}.nacInner > div{padding: 10px; cursor: pointer;}.nacInner > div:hover{background: #fafafa; color: #f60;}</style>'
+             +'    <div class="nacOuter" ng-show="showMorePkgs">'
+             +'      <div class="nacInner">'
+             +'        <div ng-repeat="item in data | filter: inputArray[inputArray.length-1]">'
+             +'          <div class="unchecked" ng-if="inputArray.indexOf(item) === -1" ng-click="set(item)"> {{item}}</div>'
+             +'          <div class="checked" ng-if="inputArray.indexOf(item) > -1" ng-click="unset(item)"><i class="glyphicon glyphicon-ok"></i> {{item}}</div>'
+             +'        </div>'
+             +'      </div>'
+             +'    </div>'
+             +'    <style>.nacContainer:hover .nacOuter{display: block;}.nacOuter{position: relative; display: none;}.nacInner{width: 100%; max-height: 250px; overflow: auto; position: absolute; left: 0; top: 0; z-index: 10; background: #fff; border: 1px solid #eee; border-top: none; box-shadow: 0 0 5px rgba(0,0,0,.1);}.nacInner div > div{padding: 10px; cursor: pointer;}.nacInner > div:hover{background: #fafafa; color: #369;}.nacInner div.checked{background:#fffaf0;border-bottom:1px dashed #eee}</style>'
              +'</div>',
     scope: {
       input: '=ngModel',
-      data: '=nacData',
+      data: '=ncData',
       type: '@type',
       placeholder: '@placeholder'
     },
     require: 'ngModel',
     link: function (scope, elem, attrs, ngModelCtrl) {
-      scope.inputArray = [];
-
-      scope.parse = function (event) {
+      scope.$watch('input', function (newVal, oldVal) {
+        if (typeof scope.input === 'undefined') {
+          scope.input = '';
+        }
         scope.inputArray = scope.input.split(',');
+      });
+
+      scope.focusInput = function () {
+        elem.find('input').focus();
       };
 
       scope.set = function (item) {
-        if (item) {
-          scope.inputArray.push(item);
+        if (scope.inputArray === []) {
+          scope.inputArray = [item];
+        } else {
+          scope.inputArray.splice(scope.inputArray.length-1,1,[item]);
         }
+        scope.inputArray = scope.inputArray.filter(function (item) {
+          return item !== '';
+        });
+        scope.input = scope.inputArray.join(',') + ',';
+      }
+
+      scope.unset = function (item) {
+        scope.inputArray.splice(scope.inputArray.indexOf(item), 1);
         scope.input = scope.inputArray.join(',');
-      };
+      }
+
+      ngModelCtrl.$parsers.push(function (viewValue) {
+        console.log('viewValue')
+      });
     }
   };
 });
